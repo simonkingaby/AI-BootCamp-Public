5ad3ee2e2ab23f355f0fe4935e9171659d614fba
Simon Kingaby <skingaby@gmail.com>
2024-03-11 16:01:56 -0400

    Create diabetes_lambda-finish.py

---
+++ Diabetes Chatbot/diabetes_lambda-finish.py
@@ -0,0 +1,256 @@
+"""
+ This code sample demonstrates an implementation of the Lex Code Hook Interface
+ in order to serve a bot.
+ Bot, Intent, and Slot models which are compatible with this sample can be found in the Lex Console as part of the 'MakeAppointment' template.
+
+ For instructions on how to set up and test this bot, as well as additional samples,
+ visit the Lex Getting Started documentation http://docs.aws.amazon.com/lex/latest/dg/getting-started.html.
+"""
+
+import time
+import os
+import logging
+import pandas as pd
+import joblib
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+""" --- Helpers to build responses which match the structure of the necessary dialog actions --- """
+
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message, response_card):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'ElicitSlot',
+            'intentName': intent_name,
+            'slots': slots,
+            'slotToElicit': slot_to_elicit,
+            'message': message,
+            'responseCard': response_card
+        }
+    }
+
+
+def confirm_intent(session_attributes, intent_name, slots, message, response_card):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'ConfirmIntent',
+            'intentName': intent_name,
+            'slots': slots,
+            'message': message,
+            'responseCard': response_card
+        }
+    }
+
+
+def close(session_attributes, fulfillment_state, message):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message
+        }
+    }
+
+    return response
+
+
+def delegate(session_attributes, slots):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Delegate',
+            'slots': slots
+        }
+    }
+
+
+def build_response_card(title, subtitle, options):
+    """
+    Build a responseCard with a title, subtitle, and an optional set of options which should be displayed as buttons.
+    """
+    buttons = None
+    if options is not None:
+        buttons = []
+        for i in range(min(5, len(options))):
+            buttons.append(options[i])
+
+    return {
+        'contentType': 'application/vnd.amazonaws.card.generic',
+        'version': 1,
+        'genericAttachments': [{
+            'title': title,
+            'subTitle': subtitle,
+            'buttons': buttons
+        }]
+    }
+
+
+def parse_int(n):
+    try:
+        return int(n)
+    except ValueError:
+        return float('nan')
+
+
+def try_ex(func):
+    """
+    Call passed in function in try block. If KeyError is encountered return None.
+    This function is intended to be used to safely access dictionary.
+
+    Note that this function would have negative impact on performance.
+    """
+
+    try:
+        return func()
+    except KeyError:
+        return None
+
+
+def get_slot_value(slots, slot_name, default_value):
+    if slot_name in slots:
+        if slots[slot_name] == None:
+            return default_value
+        else:
+            return slots[slot_name]['value']['interpretedValue']
+    else:
+        raise("Slot Name Incorrect")
+
+
+""" --- Helper Functions --- """
+
+
+""" --- Functions that control the bot's behavior --- """
+
+
+def predict_diabetes(intent_request):
+    """
+    Performs dialog management and fulfillment for predicting diabetes
+
+    Beyond fulfillment, the implementation for this intent demonstrates the following:
+    1) Use of elicitSlot in slot validation and re-prompting
+    2) Use of confirmIntent to support the confirmation of inferred slot values, when confirmation is required
+    on the bot model and the inferred slot values fully specify the intent.
+    """
+    
+    # Log the entire intent request
+    logging.debug(f"Intent:\n{intent_request}")
+    
+    session = intent_request['sessionState']
+    intent = session['intent']
+    slots = intent['slots']
+    
+    # Log the slot values
+    logging.debug(f'Slots: {slots}')
+    
+    # Parse the slot values
+    # e.g.
+    age = parse_int(get_slot_value(slots, "age", 0))
+    current_smoker = (get_slot_value(slots, "current_smoker", ""))
+    former_smoker = (get_slot_value(slots, "former_smoker", ""))
+    gender = (get_slot_value(slots, "gender", ""))
+    heart_disease = (get_slot_value(slots, "heart_disease", ""))
+    hypertension = (get_slot_value(slots, "hypertension", ""))
+    bmi = float(get_slot_value(slots, "bmi", 0))
+
+    if current_smoker == 'Yes':
+        smoking_history = 'current'
+    elif former_smoker == 'Yes':
+        smoking_history = 'former'
+    else:   
+        smoking_history = 'never'
+    if heart_disease == 'Yes':
+        heart_disease = 1
+    else:
+        heart_disease = 0
+    if hypertension == 'Yes':
+        hypertension = 1
+    else:
+        hypertension = 0
+
+    # make a DataFrame out of the gathered slots
+    X = pd.DataFrame({"gender": [gender], 
+                      "age": [age], 
+                      "hypertension": [hypertension],
+                      "heart_disease": [heart_disease],
+                      "smoking_history": [smoking_history],
+                      "bmi": [bmi]}, index=[0])
+    
+    # Now load the OHE
+    encoder = joblib.load("encoder.pkl")
+    # and encode the data
+    X = encoder.transform(X)
+
+    # Now load the scaler
+    scaler = joblib.load('scaler.pkl')
+    # and scale the data
+    X = scaler.transform(X)
+
+    # Now load the model
+    model = joblib.load('model.pkl')
+    # and predict
+    y_pred = model.predict(X)
+    if y_pred[0] == 1:
+        do_or_do_not = 'do'
+    else:
+        do_or_do_not = 'do not'
+    
+    # Return the response
+    intent['state'] = 'Fulfilled'
+    return {
+        'sessionState': {
+            'dialogAction': {
+                'type': 'Close'
+            },
+            'intent': intent
+        },
+        'requestAttributes': {},
+        'messages': [
+            {
+                'contentType': 'PlainText',
+                'content': f'As a {age} year-old {gender} with the indicators you gave, it is likely that you {do_or_do_not} have diabetes.'
+            }
+        ]
+    }
+    
+
+""" --- Intents --- """
+
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    # logger.debug('dispatch intentName={}'.format(intent_request['currentIntent']['name']))
+    
+    logger.debug(f'Intent Request {intent_request}')
+    
+    intent_name = intent_request['sessionState']['intent']['name']
+    
+    # Dispatch to your bot's intent handlers
+    if intent_name == 'PredictDiabetes':
+        return predict_diabetes(intent_request)
+    raise Exception('Intent with name ' + intent_name + ' not supported')
+
+
+""" --- Main handler --- """
+
+
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+    # By default, treat the user request as coming from the America/New_York time zone.
+    os.environ['TZ'] = 'America/New_York'
+    time.tzset()
+    logger.debug('event.bot.name={}'.format(event['bot']['name']))
+
+    return dispatch(event)
